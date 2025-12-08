c694db3181f152e603d5ac170d29d5428713a87d
giovana borges <gigi12345as17@gmail.com>
2025-12-08 17:05:57 -0300

    chore: add optional method for findbyid

--- src/main/java/com/booking/booking/repositories/ProviderAvailabilityRepository.java
+++ src/main/java/com/booking/booking/repositories/ProviderAvailabilityRepository.java
@@ -1,10 +1,12 @@
 package com.booking.booking.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.booking.booking.models.ProviderAvailability;
 
 public interface ProviderAvailabilityRepository extends JpaRepository<ProviderAvailability,Long>{
-    
+    Optional<ProviderAvailability> findById(Long id);
 }
