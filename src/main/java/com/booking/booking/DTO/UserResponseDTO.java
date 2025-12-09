503663ce186172b5ef4e7b64cd106cd1b41b7181
giovana borges <gigi12345as17@gmail.com>
2025-12-09 17:48:26 -0300

    fix: added new params of user model

--- src/main/java/com/booking/booking/DTO/UserResponseDTO.java
+++ src/main/java/com/booking/booking/DTO/UserResponseDTO.java
@@ -1,10 +1,14 @@
 package com.booking.booking.DTO;
 
+import java.time.LocalDateTime;
+
 import com.booking.booking.ENUMS.RolesENUM;
 
 public record UserResponseDTO(
     Long id,
+    String name,
     String email,
     String password,
-    RolesENUM roles
+    RolesENUM roles,
+    LocalDateTime createdAt
 ){}
