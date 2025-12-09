ce50bb75493263e41f7385adaf6f899611f5ffc2
giovana borges <gigi12345as17@gmail.com>
2025-12-09 17:48:46 -0300

    feat: added new params of user model

--- src/main/java/com/booking/booking/DTO/UserRequestDTO.java
+++ src/main/java/com/booking/booking/DTO/UserRequestDTO.java
@@ -3,6 +3,7 @@ package com.booking.booking.DTO;
 import com.booking.booking.ENUMS.RolesENUM;
 
 public record UserRequestDTO(
+    String name,
     String email,
     String password,
     RolesENUM roles
