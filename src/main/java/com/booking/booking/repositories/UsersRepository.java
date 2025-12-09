169fb017803e33143ef08bfe81fdad2b3bde3a4b
giovana borges <gigi12345as17@gmail.com>
2025-12-09 17:51:58 -0300

    feat: add method for findUserByEmail

--- src/main/java/com/booking/booking/repositories/UsersRepository.java
+++ src/main/java/com/booking/booking/repositories/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.booking.booking.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.booking.booking.models.Users;
 
 public interface UsersRepository extends JpaRepository<Users,Long>{
-    
+    Optional<Users>findByEmail(String email);
 }
