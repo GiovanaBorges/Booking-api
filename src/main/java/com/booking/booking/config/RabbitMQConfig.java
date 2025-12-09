844653fba851336753ae68fb7b442ea092a26f45
giovana borges <gigi12345as17@gmail.com>
2025-12-09 17:51:13 -0300

    chore: remove unused import

--- src/main/java/com/booking/booking/config/RabbitMQConfig.java
+++ src/main/java/com/booking/booking/config/RabbitMQConfig.java
@@ -8,8 +8,7 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
+import org.springframework.context.annotation.Profile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
