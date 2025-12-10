0279d4909d44cb17bc580e01d71af04e7a466b38
giovana borges <gigi12345as17@gmail.com>
2025-12-09 21:17:19 -0300

    feat: add bookingUpdatedEvent for rabbitmq use

---
+++ src/main/java/com/booking/booking/events/bookingEvents/BookingUpdatedEvent.java
@@ -0,0 +1,17 @@
+package com.booking.booking.events.bookingEvents;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BookingUpdatedEvent {
+    private Long id;
+    private Long providerId;
+    private Long customerId;
+    private LocalDateTime  startsTs;
+    private LocalDateTime  endTs;
+    private LocalDateTime eventTs = LocalDateTime.now();
+}
