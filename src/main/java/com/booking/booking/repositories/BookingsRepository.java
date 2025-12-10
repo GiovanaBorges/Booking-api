c35406356d36f315827b61f0a2d24e1d88023e84
giovana borges <gigi12345as17@gmail.com>
2025-12-09 21:18:24 -0300

    feat: add method for conflict bookings

--- src/main/java/com/booking/booking/repositories/BookingsRepository.java
+++ src/main/java/com/booking/booking/repositories/BookingsRepository.java
@@ -1,9 +1,24 @@
 package com.booking.booking.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.booking.booking.models.Bookings;
+import java.time.LocalDateTime;
+
+
 
 public interface BookingsRepository extends JpaRepository<Bookings,Long>{
-    
+    @Query("""
+            SELECT b FROM Bookings b
+            WHERE b.provider.id = :providerId
+            AND(
+                (b.startTs <= :endTs AND b.endTs >= :startTs)
+            )
+            """)
+    Optional<Bookings> findConflict(Long providerId,LocalDateTime startsTs,LocalDateTime endTs);
+
+    Optional<Bookings>findById(Long id);
 }
