7843c80b84fbe5637db37f4bcdb05fe7d4532a54
giovana borges <gigi12345as17@gmail.com>
2025-12-22 10:38:35 -0300

    feat: add idempotency feature for  provider register

--- src/main/java/com/booking/booking/controllers/ProviderAvailabilityController.java
+++ src/main/java/com/booking/booking/controllers/ProviderAvailabilityController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,9 @@ public class ProviderAvailabilityController {
     private ProviderAvailabilityservices services;
     
     @PostMapping("/register")
-    public ResponseEntity<ProviderAvailabilityResponseDTO> registerProviderAvailability(@RequestBody ProviderAvailabilityRequestDTO request) {
+    public ResponseEntity<ProviderAvailabilityResponseDTO> registerProviderAvailability(
+        @RequestBody ProviderAvailabilityRequestDTO request,
+        @RequestHeader("Idempotency-Key") String key) {
         return ResponseEntity.ok().body(services.saveProviderAvailability(request));
     }
 
