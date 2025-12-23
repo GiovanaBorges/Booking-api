0cb161f5c47f74e0c737a0688817a651727cd6d8
giovana borges <gigi12345as17@gmail.com>
2025-12-23 18:04:21 -0300

    fix: config for integration tests

--- src/main/java/com/booking/booking/config/RabbitMQConfig.java
+++ src/main/java/com/booking/booking/config/RabbitMQConfig.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.amqp.core.*;
 
+@Profile("!test")
 @Configuration
 public class RabbitMQConfig {
     private static final Logger log = LoggerFactory.getLogger(RabbitMQConfig.class);
