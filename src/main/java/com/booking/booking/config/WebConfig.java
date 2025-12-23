60e8e11b17a1888574b598362fb053ad609f54ca
giovana borges <gigi12345as17@gmail.com>
2025-12-23 18:23:29 -0300

    config: config for idempotency

---
+++ src/main/java/com/booking/booking/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.booking.booking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final IdempotencyInterceptor interceptor;
+
+    public WebConfig(IdempotencyInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+}
