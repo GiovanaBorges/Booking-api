ba153e1561b210695dc718351fda53468f1559eb
giovana borges <gigi12345as17@gmail.com>
2026-03-05 19:16:46 -0300

    chore: add props

--- src/main/java/com/booking/booking/BookingApplication.java
+++ src/main/java/com/booking/booking/BookingApplication.java
@@ -2,8 +2,12 @@ package com.booking.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.booking.booking.config.rabbitmq.RabbitMQProperties;
+
+@EnableConfigurationProperties(RabbitMQProperties.class)
 @SpringBootApplication
 @EnableJpaAuditing
 public class BookingApplication {
