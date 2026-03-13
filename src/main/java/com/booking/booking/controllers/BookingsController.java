d4dd761c4a109699c45eb8e379ab29c7e6a6664a
giovana borges <gigi12345as17@gmail.com>
2026-03-13 12:22:19 -0300

    refactor: remove unused import

--- src/main/java/com/booking/booking/controllers/BookingsController.java
+++ src/main/java/com/booking/booking/controllers/BookingsController.java
@@ -13,7 +13,6 @@ import com.booking.booking.services.BookingsServices;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
