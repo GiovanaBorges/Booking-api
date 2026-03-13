51805c959e73abd599f9bde147f25da6486bb2a8
giovana borges <gigi12345as17@gmail.com>
2026-03-13 12:24:29 -0300

    fix: add anotation mapper

--- src/main/java/com/booking/booking/mappers/ProviderAvailabilityMapper.java
+++ src/main/java/com/booking/booking/mappers/ProviderAvailabilityMapper.java
@@ -1,6 +1,9 @@
 package com.booking.booking.mappers;
 
 
+import java.util.Optional;
+
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
@@ -8,6 +11,7 @@ import com.booking.booking.DTO.requests.ProviderAvailabilityRequestDTO;
 import com.booking.booking.DTO.responses.ProviderAvailabilityResponseDTO;
 import com.booking.booking.models.ProviderAvailability;
 
+@Mapper(componentModel = "spring")
 public interface ProviderAvailabilityMapper {
     @Mapping(target = "provider", ignore = true)
     ProviderAvailability toEntity(ProviderAvailabilityRequestDTO requestDTO);
