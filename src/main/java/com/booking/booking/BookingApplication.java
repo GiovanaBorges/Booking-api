7e296d2b624983e465a7de3cc8ba260bf2e317af
giovana borges <gigi12345as17@gmail.com>
2026-03-13 11:55:53 -0300

    fix: remove rabbitmqproperties annotation

--- src/main/java/com/booking/booking/BookingApplication.java
+++ src/main/java/com/booking/booking/BookingApplication.java
@@ -2,12 +2,8 @@ package com.booking.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.booking.booking.config.rabbitmq.RabbitMQProperties;
-
-@EnableConfigurationProperties(RabbitMQProperties.class)
 @SpringBootApplication
 @EnableJpaAuditing
 public class BookingApplication {
