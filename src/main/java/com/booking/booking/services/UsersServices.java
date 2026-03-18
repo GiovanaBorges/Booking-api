1608603099b373286b0a63edf53ddb5023b8a81e
giovana borges <gigi12345as17@gmail.com>
2026-03-18 18:26:43 -0300

    feat: add updateUser , findbyid and findbySkill

--- src/main/java/com/booking/booking/services/UsersServices.java
+++ src/main/java/com/booking/booking/services/UsersServices.java
@@ -1,6 +1,9 @@
 package com.booking.booking.services;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,19 +11,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.booking.booking.DTO.requests.UserRequestDTO;
 import com.booking.booking.DTO.responses.UserResponseDTO;
 import com.booking.booking.ENUMS.RolesENUM;
+import com.booking.booking.ENUMS.TechSkillsENUM;
 import com.booking.booking.exceptions.ApiException;
 import com.booking.booking.mappers.UserMapper;
 import com.booking.booking.mappers.events.UserEventMapper;
 import com.booking.booking.models.Users;
 import com.booking.booking.repositories.UsersRepository;
+import com.booking.booking.services.helpers.UserFallbackFactory;
+import com.booking.booking.services.helpers.UserResolver;
 import com.booking.booking.services.rabbitMQEvents.MessageProducerUsers;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Service
 public class UsersServices {
@@ -35,7 +44,13 @@ public class UsersServices {
     private UserMapper mapperUser;
 
     @Autowired
+    private UserResolver userResolver;
+
+    @Autowired
     private UserEventMapper userEventMapper;
+
+    @Autowired
+    private UserFallbackFactory userFallbackFactory;
 
     private static final Logger LOG =
     LoggerFactory.getLogger(BookingsServices.class);
@@ -71,6 +86,51 @@ public class UsersServices {
         return mapperUser.toResponse(user);
     }
 
+    @Bulkhead(name = "userBulkead")
+    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @RateLimiter(name = "userRateLimiter")
+    @Retry(name = "userRetry", fallbackMethod = "handleUserEventFailure")
+    public List<UserResponseDTO> getUsersBySkill(TechSkillsENUM skill) {
+        List<Users> usersWithSkill = usersRepository.findUsersBySkill(skill);
+
+        // Transformando para DTO (ex: sem senha, só info públicas)
+        return usersWithSkill.stream()
+                .map(user -> new UserResponseDTO(
+                        user.getId(),
+                        user.getName(),
+                        user.getDescription(),
+                        user.getRoles(), 
+                        user.getLinkedinProfile(),
+                        user.getGithubProfile(),
+                        user.getPortfolioUrl(),
+                        user.getPortfolioUrl(), 
+                        user.getExperienceYears(),
+                        user.getSkills(), 
+                        user.getCreatedAt()
+                ))
+                .collect(Collectors.toList());
+    }
+ 
+    @Bulkhead(name = "userBulkead")
+    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @RateLimiter(name = "userRateLimiter")
+    @Transactional
+    public UserResponseDTO editUser(Long id, UserRequestDTO dto) {
+        Users existingUser = userResolver.resolveUserById(id);
+
+        Users updated = usersRepository.save(existingUser);
+        userEventMapper.toUpdatedEvent(updated);
+        return mapperUser.toResponse(updated);
+    }
+
+    @Bulkhead(name = "userBulkead")
+    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @RateLimiter(name = "userRateLimiter")
+    @Retry(name = "userRetry", fallbackMethod = "handleUserEventFailure")
+    public UserResponseDTO getUserById(Long id){
+        return mapperUser.toResponse(userResolver.resolveUserById(id));
+    }
+
      // ============================
     // FALLBACK METHODS
     // ============================
@@ -84,6 +144,22 @@ public class UsersServices {
     }
 
     public UserResponseDTO handleUserEventFailure(Jwt jwt, Throwable t){
-        throw serviceUnavailable(t);
+        LOG.error("Fallback triggered for createUser jwt={}", jwt, t);
+        return userFallbackFactory.createFallbackFromJwt(jwt.getSubject());
+    }
+
+    public UserResponseDTO handleUserEventFailure(Long id, Throwable t){
+        LOG.error("Fallback triggered for getUserById id={}", id, t);
+        return userFallbackFactory.createFallback(id);
+    }
+
+    public UserResponseDTO handleUserEventFailure(Long id, UserRequestDTO dto, Throwable t){
+        LOG.error("Fallback triggered for editUser dto={}", dto, t);
+        return userFallbackFactory.createFallbackForEdit(id);
+    }
+
+    public List<UserResponseDTO> handleUserEventFailure(TechSkillsENUM skill, Throwable t){
+        LOG.error("Fallback triggered for getUsersBySkill skill={}", skill, t);
+        return userFallbackFactory.createFallbackForSkill(skill);
     }
 }
