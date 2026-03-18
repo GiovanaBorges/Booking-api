741ecccfebf7031a2a8e3e34345887052aa05cb7
giovana borges <gigi12345as17@gmail.com>
2026-03-18 18:20:51 -0300

    refactor: add new atributes of  user

--- src/main/java/com/booking/booking/DTO/requests/UserRequestDTO.java
+++ src/main/java/com/booking/booking/DTO/requests/UserRequestDTO.java
@@ -1,7 +1,23 @@
 package com.booking.booking.DTO.requests;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.validator.constraints.URL;
+
 import com.booking.booking.ENUMS.RolesENUM;
+import com.booking.booking.ENUMS.TechSkillsENUM;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Enumerated;
 
 public record UserRequestDTO(
-    String name
-) {}
+    String name,
+    String Description,
+    String linkedinProfile,
+    String githubProfile,
+    String portfolioUrl,
+    Integer experienceYears,
+    Set<TechSkillsENUM> skills
+){}
