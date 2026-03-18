251082e5df4cf6bf6f7b2cd133dec4efae0506f5
giovana borges <gigi12345as17@gmail.com>
2026-03-18 18:20:31 -0300

    refactor: add title , description

--- src/main/java/com/booking/booking/DTO/requests/BookingsRequestDTO.java
+++ src/main/java/com/booking/booking/DTO/requests/BookingsRequestDTO.java
@@ -3,10 +3,14 @@ package com.booking.booking.DTO.requests;
 import java.time.LocalDateTime;
 import com.booking.booking.ENUMS.StatusENUM;
 
+import jakarta.persistence.Column;
+
 public record BookingsRequestDTO(
     Long providerId,
     Long customerId,
     LocalDateTime startsTs,
     LocalDateTime endTs,
-    StatusENUM status
+    StatusENUM status,
+    String title,
+    String description
 ) {}
