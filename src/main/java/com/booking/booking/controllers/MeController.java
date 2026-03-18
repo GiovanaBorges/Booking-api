35af120d4296f70b46b05f26ead18cd60fef981b
giovana borges <gigi12345as17@gmail.com>
2026-03-18 18:19:55 -0300

    feat: add methods getById/skill and update user

--- src/main/java/com/booking/booking/controllers/MeController.java
+++ src/main/java/com/booking/booking/controllers/MeController.java
@@ -1,13 +1,21 @@
 package com.booking.booking.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.booking.booking.DTO.requests.UserRequestDTO;
 import com.booking.booking.DTO.responses.UserResponseDTO;
+import com.booking.booking.ENUMS.TechSkillsENUM;
 import com.booking.booking.services.UsersServices;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -18,8 +26,23 @@ public class MeController {
     private UsersServices usersServices;
 
     @GetMapping("/me")
-    public UserResponseDTO me(JwtAuthenticationToken auth) {
-        return usersServices.createOrGet(auth.getToken());
+    public ResponseEntity<UserResponseDTO> me(JwtAuthenticationToken auth) {
+        return ResponseEntity.ok(usersServices.createOrGet(auth.getToken()));
+    }
+    
+    @GetMapping("/skill/{skill}")
+    public ResponseEntity<List<UserResponseDTO>> getUsersBySkill(@PathVariable TechSkillsENUM skill) {
+        return ResponseEntity.ok(usersServices.getUsersBySkill(skill));
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<UserResponseDTO> editUsers(@PathVariable Long id, @RequestBody UserRequestDTO dto) {
+        return ResponseEntity.ok(usersServices.editUser(id, dto));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(usersServices.getUserById(id));
     }
     
     
