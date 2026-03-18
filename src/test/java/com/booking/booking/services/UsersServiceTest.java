222e4bc8ef7cba49e5a5bd69d89935b950ca7269
giovana borges <gigi12345as17@gmail.com>
2026-03-18 18:29:45 -0300

    refactor: add new atributes for user

--- src/test/java/com/booking/booking/services/UsersServiceTest.java
+++ src/test/java/com/booking/booking/services/UsersServiceTest.java
@@ -3,6 +3,7 @@ package com.booking.booking.services;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -10,7 +11,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,13 +23,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import com.booking.booking.DTO.requests.UserRequestDTO;
 import com.booking.booking.DTO.responses.UserResponseDTO;
 import com.booking.booking.ENUMS.RolesENUM;
+import com.booking.booking.ENUMS.TechSkillsENUM;
 import com.booking.booking.events.usersEvents.UsersCreatedEvent;
 import com.booking.booking.mappers.UserMapper;
 import com.booking.booking.mappers.events.UserEventMapper;
 import com.booking.booking.models.Users;
 import com.booking.booking.repositories.UsersRepository;
+import com.booking.booking.services.helpers.UserFallbackFactory;
+import com.booking.booking.services.helpers.UserResolver;
 import com.booking.booking.services.rabbitMQEvents.MessageProducerUsers;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,6 +54,12 @@ public class UsersServiceTest {
     @Mock
     private UserEventMapper userEventMapper;
 
+    @Mock 
+    private UserResolver userResolver;
+
+    @Mock
+    private UserFallbackFactory userFallbackFactory;
+
     @Test
     void shouldReturnExistingUser(){
 
@@ -60,12 +74,20 @@ public class UsersServiceTest {
             .keycloakId("keycloakId123")
             .email("email@email.com")
             .name("user1")
+            .skills(Set.of(TechSkillsENUM.ANGULAR,TechSkillsENUM.AI))
             .roles(RolesENUM.PROVIDER)
             .createdAt(LocalDateTime.now())
             .build();
 
         UserResponseDTO dto =
-                new UserResponseDTO(1L, "user1", "email@email.com", RolesENUM.PROVIDER, existingUser.getCreatedAt());
+                new UserResponseDTO(1L, "user1", "email@email.com", RolesENUM.PROVIDER, 
+                "Description", 
+                "http://linkedin.com/in/user1", 
+                "http://github.com/user1", 
+                "http://twitter.com/user1", 
+                2, 
+                 existingUser.getSkills(), 
+                existingUser.getCreatedAt());
 
         when(repository.findByKeycloakId("keycloakId123"))
             .thenReturn(Optional.of(existingUser));
@@ -125,7 +147,18 @@ public class UsersServiceTest {
                 .thenReturn(event);
 
         UserResponseDTO dto =
-                new UserResponseDTO(1L, "user1", "email@email.com", RolesENUM.ADMIN, LocalDateTime.now());
+                new UserResponseDTO(
+                    1L, 
+                    "user1",
+                 "email@email.com", 
+                RolesENUM.ADMIN, 
+                "Description", 
+                "http://linkedin.com/in/user1", 
+                "http://github.com/user1", 
+                "http://twitter.com/user1", 
+                2, 
+                Set.of(TechSkillsENUM.ANGULAR,TechSkillsENUM.AI),
+                LocalDateTime.now());
 
         when(mapperUser.toResponse(any()))
                 .thenReturn(dto);
@@ -144,6 +177,118 @@ public class UsersServiceTest {
         verify(repository, times(1)).save(any(Users.class));
         verify(messageProducerUsers, times(1))
             .sendUsersCreateEvent(any(UsersCreatedEvent.class));
+    }
+
+    // ===============================
+    // Test editUser
+    // ===============================
+    @Test
+    void shouldEditExistingUser() {
+        Users existingUser = Users.builder()
+                .id(1L)
+                .name("User One")
+                .email("user@email.com")
+                .Description("Old desc")
+                .skills(new HashSet<>(Set.of(TechSkillsENUM.JAVA, TechSkillsENUM.REACT)))
+                .roles(RolesENUM.PROVIDER)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        UserRequestDTO dto = new UserRequestDTO(
+                "New Name",              
+                "new@email.com",          
+                "New desc",               
+                "linkedin.com/new",        
+                "github.com/new",       
+                5,                        
+                Set.of(TechSkillsENUM.ANGULAR, TechSkillsENUM.AI) 
+        );
+
+        when(userResolver.resolveUserById(1L)).thenReturn(existingUser);
+
+        when(repository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        when(mapperUser.toResponse(any())).thenAnswer(invocation -> {
+            Users u = invocation.getArgument(0);
+            return new UserResponseDTO(
+                u.getId(),
+                u.getName(),
+                u.getEmail(),
+                u.getRoles(),
+                u.getDescription(),
+                u.getLinkedinProfile(),
+                u.getGithubProfile(),
+                u.getPortfolioUrl(),
+                u.getExperienceYears(),
+                u.getSkills(),
+                u.getCreatedAt()
+            );
+        });
+
+        // Chama o service
+        UserResponseDTO response = service.editUser(1L, dto);
+
+        assertEquals(1L, response.id());
+
+        verify(repository, times(1)).save(existingUser);
+        verify(userEventMapper, times(1)).toUpdatedEvent(existingUser);
+    }
+
+    // ===============================
+    // Test getUserById com fallback
+    // ===============================
+    @Test
+    void shouldReturnUserById() {
+        Users existingUser = Users.builder()
+                .id(1L)
+                .name("User One")
+                .email("user@email.com")
+                .skills(new HashSet<>(Set.of(TechSkillsENUM.JAVA, TechSkillsENUM.REACT)))
+                .roles(RolesENUM.PROVIDER)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+            when(userResolver.resolveUserById(1L)).thenReturn(existingUser);
+
+            when(mapperUser.toResponse(existingUser)).thenReturn(
+                    new UserResponseDTO(
+                        existingUser.getId(),
+                        existingUser.getName(),
+                        existingUser.getDescription(),
+                        existingUser.getRoles(), 
+                        existingUser.getLinkedinProfile(),
+                        existingUser.getGithubProfile(),
+                        existingUser.getPortfolioUrl(),
+                        existingUser.getPortfolioUrl(),
+                        existingUser.getExperienceYears(), 
+                        existingUser.getSkills(), 
+                        existingUser.getCreatedAt()
+                    )
+            );
+        UserResponseDTO response = service.getUserById(1L);
+
+        assertAll(
+                () -> assertEquals(1L, response.id()),
+                () -> assertEquals("User One", response.name()),
+                () -> assertEquals(2, response.skills().size())
+        );
+
+        verify(userResolver, times(1)).resolveUserById(1L);
+        verify(mapperUser, times(1)).toResponse(existingUser);
+    }
+
+     @Test
+    void shouldReturnFallbackUserWhenGetUserByIdFails() {
+        lenient().when(userResolver.resolveUserById(2L)).thenThrow(new RuntimeException("DB down"));
+        lenient().when(userFallbackFactory.createFallback(2L)).thenCallRealMethod();
+
+        UserResponseDTO fallback = service.handleUserEventFailure(2L, new RuntimeException());
+
+        assertAll(
+            () -> assertEquals(2L, fallback.id()),
+            () -> assertEquals("USER SERVICE TEMPORARILY UNAVAILABLE", fallback.name()),
+            () -> assertEquals(0, fallback.skills().size())
+        );
     }
 
 }
