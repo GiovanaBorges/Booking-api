79e069d583a23e82c35c1e14bd5f8aa8fe35f08d
giovana borges <gigi12345as17@gmail.com>
2026-03-30 19:35:04 -0300

    refactor: DI by constructor and fix some methods

--- src/main/java/com/booking/booking/services/UsersServices.java
+++ src/main/java/com/booking/booking/services/UsersServices.java
@@ -1,22 +1,25 @@
 package com.booking.booking.services;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.booking.booking.DTO.EventDTO;
 import com.booking.booking.DTO.requests.UserRequestDTO;
 import com.booking.booking.DTO.responses.UserResponseDTO;
+import com.booking.booking.ENUMS.EventTypeEnum;
 import com.booking.booking.ENUMS.RolesENUM;
 import com.booking.booking.ENUMS.TechSkillsENUM;
+import com.booking.booking.events.usersEvents.UsersCreatedEvent;
+import com.booking.booking.events.usersEvents.UsersUpdatedEvent;
 import com.booking.booking.exceptions.ApiException;
 import com.booking.booking.mappers.UserMapper;
 import com.booking.booking.mappers.events.UserEventMapper;
@@ -30,135 +33,186 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @Service
 public class UsersServices {
 
-    @Autowired
-    private UsersRepository usersRepository;
+    private final UsersRepository usersRepository;
+    private final MessageProducerUsers messageProducerUsers;
+    private final UserMapper mapperUser;
+    private final UserResolver userResolver;
+    private final UserEventMapper userEventMapper;
+    private final UserFallbackFactory userFallbackFactory;
+    private final KeycloakService keycloakService;
 
-    @Autowired
-    private MessageProducerUsers messageProducerUsers;
+    private static final Logger LOG = LoggerFactory.getLogger(BookingsServices.class);
 
-    @Autowired
-    private UserMapper mapperUser;
-
-    @Autowired
-    private UserResolver userResolver;
-
-    @Autowired
-    private UserEventMapper userEventMapper;
-
-    @Autowired
-    private UserFallbackFactory userFallbackFactory;
-
-    private static final Logger LOG =
-    LoggerFactory.getLogger(BookingsServices.class);
-
-    @Bulkhead(name = "userBulkead")
-    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @Bulkhead(name = "userBulkhead")
+    @CircuitBreaker(name = "userCircuitBreaker")
     @RateLimiter(name = "userRateLimiter")
-    public UserResponseDTO createOrGet(Jwt jwt) {
+    @Transactional
+    public Users createOrGet(Jwt jwt) {
 
         String keycloakId = jwt.getSubject();
         String email = jwt.getClaim("email");
         String name = jwt.getClaim("preferred_username");
+        String accountType = jwt.getClaim("account_type");
 
-        RolesENUM role = RolesENUM.ADMIN;
         Users user = usersRepository.findByKeycloakId(keycloakId)
                 .orElseGet(() -> {
+                    System.out.println("NAO TEM CONTA AINDA");
 
-                    Users newUser = Users.builder()
-                            .keycloakId(keycloakId)
-                            .email(email)
-                            .name(name)
-                            .roles(role)
-                            .createdAt(LocalDateTime.now())
-                            .build();
+                    return usersRepository.findByEmail(email)
+                            .map(existingUser -> {
+                                existingUser.setKeycloakId(keycloakId);
+                                return usersRepository.save(existingUser);
+                            })
+                            .orElseGet(() -> {
 
-                    Users savedUser = usersRepository.save(newUser);
+                                // usa o token
+                                RolesENUM role = "provider".equals(accountType)
+                                        ? RolesENUM.PROVIDER
+                                        : RolesENUM.CLIENT;
 
-                    messageProducerUsers.sendUsersCreateEvent(userEventMapper.toCreateEvent(savedUser));
+                                Users newUser = Users.builder()
+                                        .keycloakId(keycloakId)
+                                        .email(email)
+                                        .name(name)
+                                        .roles(role)
+                                        .createdAt(LocalDateTime.now())
+                                        .build();
 
-                    return savedUser;
+                                return usersRepository.save(newUser);
+                            });
                 });
 
-        return mapperUser.toResponse(user);
+        RolesENUM roleFromDb = user.getRoles();
+
+        final String groupName;
+        final String roleName;
+
+        if (roleFromDb == RolesENUM.PROVIDER) {
+            groupName = "providers";
+            roleName = "PROVIDER";
+        } else {
+            groupName = "clients";
+            roleName = "CLIENT";
+        }
+
+        keycloakService.assignGroup(keycloakId, groupName);
+        keycloakService.assignRoleToUser(keycloakId, roleName);
+
+        return user;
     }
 
     @Bulkhead(name = "userBulkead")
-    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @CircuitBreaker(name = "userCircuitBreaker")
     @RateLimiter(name = "userRateLimiter")
-    @Retry(name = "userRetry", fallbackMethod = "handleUserEventFailure")
+    @Retry(name = "userRetry")
     public List<UserResponseDTO> getUsersBySkill(TechSkillsENUM skill) {
         List<Users> usersWithSkill = usersRepository.findUsersBySkill(skill);
 
-        // Transformando para DTO (ex: sem senha, só info públicas)
+        // Transformando para DTO exexemplo: sem senha, só info públicas
         return usersWithSkill.stream()
                 .map(user -> new UserResponseDTO(
                         user.getId(),
                         user.getName(),
                         user.getDescription(),
-                        user.getRoles(), 
+                        user.getRoles(),
                         user.getLinkedinProfile(),
                         user.getGithubProfile(),
                         user.getPortfolioUrl(),
-                        user.getPortfolioUrl(), 
+                        user.getPortfolioUrl(),
                         user.getExperienceYears(),
-                        user.getSkills(), 
-                        user.getCreatedAt()
-                ))
+                        user.getSkills(),
+                        user.getCreatedAt()))
                 .collect(Collectors.toList());
     }
- 
+
     @Bulkhead(name = "userBulkead")
-    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @CircuitBreaker(name = "userCircuitBreaker")
     @RateLimiter(name = "userRateLimiter")
     @Transactional
-    public UserResponseDTO editUser(Long id, UserRequestDTO dto) {
-        Users existingUser = userResolver.resolveUserById(id);
+    public UserResponseDTO editUser(UserRequestDTO dto, Jwt jwt) {
+        String keycloakId = jwt.getSubject();
 
-        Users updated = usersRepository.save(existingUser);
-        userEventMapper.toUpdatedEvent(updated);
+        Users user = usersRepository.findByKeycloakId(keycloakId)
+                .orElseThrow(() -> new ApiException("USER NOT FOUND", HttpStatus.NOT_FOUND));
+
+        mapperUser.updateEntity(dto, user);
+
+        Users updated = usersRepository.save(user);
+
+        EventDTO<UsersUpdatedEvent> event = new EventDTO<>(
+                EventTypeEnum.USER_UPDATED,
+                userEventMapper.toUpdatedEvent(updated),
+                LocalDateTime.now(),
+                List.of(updated.getId()));
+
+        messageProducerUsers.sendEvent(event);
+
         return mapperUser.toResponse(updated);
     }
 
     @Bulkhead(name = "userBulkead")
-    @CircuitBreaker(name = "userCircuitBreaker", fallbackMethod = "handleUserEventFailure")
+    @CircuitBreaker(name = "userCircuitBreaker")
     @RateLimiter(name = "userRateLimiter")
-    @Retry(name = "userRetry", fallbackMethod = "handleUserEventFailure")
-    public UserResponseDTO getUserById(Long id){
+    @Retry(name = "userRetry")
+    public UserResponseDTO getUserById(Long id) {
         return mapperUser.toResponse(userResolver.resolveUserById(id));
     }
 
-     // ============================
+    @Bulkhead(name = "userBulkead")
+    @CircuitBreaker(name = "userCircuitBreaker")
+    @RateLimiter(name = "userRateLimiter")
+    @Retry(name = "userRetry")
+    public List<UserResponseDTO> getAllProviders() {
+
+        List<Users> providers = usersRepository.findByRoles(RolesENUM.PROVIDER);
+
+        if (providers.isEmpty()) {
+            throw new ApiException("NO PROVIDERS FOUND", HttpStatus.NOT_FOUND);
+        }
+
+        return providers.stream()
+                .map(mapperUser::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    // ============================
     // FALLBACK METHODS
     // ============================
 
-    private RuntimeException serviceUnavailable(Throwable t){
+    private RuntimeException serviceUnavailable(Throwable t) {
         LOG.error("User service fallback triggered", t);
         return new ApiException(
-            "USER SERVICE TEMPORARILY UNAVAILABLE",
-            HttpStatus.SERVICE_UNAVAILABLE
-        );
+                "USER SERVICE TEMPORARILY UNAVAILABLE",
+                HttpStatus.SERVICE_UNAVAILABLE);
     }
 
-    public UserResponseDTO handleUserEventFailure(Jwt jwt, Throwable t){
+    public UserResponseDTO handleUserEventFailure(Jwt jwt, Throwable t) {
         LOG.error("Fallback triggered for createUser jwt={}", jwt, t);
         return userFallbackFactory.createFallbackFromJwt(jwt.getSubject());
     }
 
-    public UserResponseDTO handleUserEventFailure(Long id, Throwable t){
+    public UserResponseDTO handleUserEventFailure(Long id, Throwable t) {
         LOG.error("Fallback triggered for getUserById id={}", id, t);
         return userFallbackFactory.createFallback(id);
     }
 
-    public UserResponseDTO handleUserEventFailure(Long id, UserRequestDTO dto, Throwable t){
+    public List<UserResponseDTO> handleUserEventFailure(Throwable t) {
+        LOG.error("Fallback triggered for getAllUsers", t);
+        return userFallbackFactory.createFallbackForGetAllProviders();
+    }
+
+    public UserResponseDTO handleUserEventFailure(Long id, UserRequestDTO dto, Throwable t) {
         LOG.error("Fallback triggered for editUser dto={}", dto, t);
         return userFallbackFactory.createFallbackForEdit(id);
     }
 
-    public List<UserResponseDTO> handleUserEventFailure(TechSkillsENUM skill, Throwable t){
+    public List<UserResponseDTO> handleUserEventFailure(TechSkillsENUM skill, Throwable t) {
         LOG.error("Fallback triggered for getUsersBySkill skill={}", skill, t);
         return userFallbackFactory.createFallbackForSkill(skill);
     }
