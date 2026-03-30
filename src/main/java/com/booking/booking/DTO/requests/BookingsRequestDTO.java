6ae8a5a41e56e44450b272f0b57abaf54bb64243
giovana borges <gigi12345as17@gmail.com>
2026-03-30 19:19:34 -0300

    refacotr: fix nomeclature atribute and add jsonproperty annotation

--- src/main/java/com/booking/booking/DTO/requests/BookingsRequestDTO.java
+++ src/main/java/com/booking/booking/DTO/requests/BookingsRequestDTO.java
@@ -2,13 +2,16 @@ package com.booking.booking.DTO.requests;
 
 import java.time.LocalDateTime;
 import com.booking.booking.ENUMS.StatusENUM;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 
 public record BookingsRequestDTO(
     Long providerId,
     Long customerId,
-    LocalDateTime startsTs,
+    @JsonProperty("start_ts")
+    LocalDateTime startTs,
+    @JsonProperty("end_ts")
     LocalDateTime endTs,
     StatusENUM status,
     String title,
