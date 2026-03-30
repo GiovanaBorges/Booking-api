cd6d1785b294c7438152bad370ce325602d4dd73
giovana borges <gigi12345as17@gmail.com>
2026-03-30 19:18:26 -0300

    feat: add getbookingbyprovider , status and id

--- src/main/java/com/booking/booking/controllers/BookingsController.java
+++ src/main/java/com/booking/booking/controllers/BookingsController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.booking.booking.DTO.requests.BookingsRequestDTO;
 import com.booking.booking.DTO.responses.BookingsResponseDTO;
+import com.booking.booking.DTO.responses.ProviderAvailabilityResponseDTO;
 import com.booking.booking.services.BookingsServices;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +20,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
 @RestController
 @RequestMapping("/bookings")
 public class BookingsController {
-    
+
     @Autowired
     private BookingsServices bookingsServices;
 
@@ -36,19 +37,41 @@ public class BookingsController {
         return ResponseEntity.ok().body(bookingsServices.getAllBookings());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping
+    public List<BookingsResponseDTO> getBookings(
+            @RequestParam(required = false) String role) {
+        if ("provider".equalsIgnoreCase(role)) {
+            return bookingsServices.getBookingsAsProvider();
+        }
+
+        return bookingsServices.getBookingsAsCustomer();
+    }
+
+    @GetMapping("/provider")
+    public ResponseEntity<List<BookingsResponseDTO>> getBookingsByProviderAndStatus(
+            @RequestParam String status) {
+        return ResponseEntity.ok().body(bookingsServices.getBookingsByStatus(status));
+    }
+
+    @PutMapping("/confirm/{id}")
+    public void confirm(@PathVariable Long id) {
+        bookingsServices.confirmBooking(id);
+    }
+
+    @GetMapping("/id/{id}")
     public ResponseEntity<BookingsResponseDTO> getBookingsByID(@PathVariable Long id) {
         return ResponseEntity.ok().body(bookingsServices.getBookingById(id));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<BookingsResponseDTO> deleteBookings(@PathVariable Long id){
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<BookingsResponseDTO> deleteBookings(@PathVariable Long id) {
         return ResponseEntity.ok().body(bookingsServices.deleteBooking(id));
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<BookingsResponseDTO> updateBookings(@PathVariable Long id, @RequestBody BookingsRequestDTO bookingRequestDTO) {
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<BookingsResponseDTO> updateBookings(@PathVariable Long id,
+            @RequestBody BookingsRequestDTO bookingRequestDTO) {
         return ResponseEntity.ok().body(bookingsServices.updateBooking(id, bookingRequestDTO));
     }
-    
+
 }
