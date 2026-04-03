95e6bdc53ee6a949e5edbf230a294522253a781e
giovana borges <gigi12345as17@gmail.com>
2026-04-03 10:17:38 -0300

    feat: add requestmatcher for swagger

--- src/main/java/com/booking/booking/security/SecurityConfig.java
+++ src/main/java/com/booking/booking/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -20,23 +21,29 @@ public class SecurityConfig {
     SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
         this.corsConfigurationSource = corsConfigurationSource;
     }
-    
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource))
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/**").permitAll()
-                .requestMatchers("/provideravailability/**").authenticated()
-                .requestMatchers("/users/me").authenticated()
-                .requestMatchers("/bookings/**").authenticated()
-                .requestMatchers(HttpMethod.PUT, "/users/edit/**").authenticated()  
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-            );
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/v3/api-docs/swagger-config",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/documentation/**")
+                        .permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/provideravailability/**").authenticated()
+                        .requestMatchers("/users/me").authenticated()
+                        .requestMatchers("/bookings/**").authenticated()
+
+                        .requestMatchers(HttpMethod.PUT, "/users/edit/**").authenticated()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
     }
 
@@ -47,5 +54,5 @@ public class SecurityConfig {
         return converter;
     }
 
-    
+
 }
